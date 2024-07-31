7ee4093fff5eae1caee8a48e05847ece98f56b63
PranayPROYAL <139565048+PranayPROYAL@users.noreply.github.com>
2024-07-31 21:37:18 +0530

    Add files via upload

---
+++ week1Design Patterns/Excercise4/PayPalGateway.java
@@ -0,0 +1,9 @@
+package Exercise4;
+
+// File: PayPalGateway.java
+public class PayPalGateway {
+    public void sendPayment(double amount) {
+        System.out.println("Processing payment of Rs" + amount + " through PayPal.");
+    }
+}
+
